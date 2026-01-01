dd97029e35400182508eff0d4113960005228d63
36132 <36132@iqraisb.edu.pk>
2026-01-01 21:35:53 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function togglePara() {
+  var paragraph = document.getElementById("para");
+
+  if (paragraph.style.display === "none") {
+    paragraph.style.display = "block";
+  } else {
+    paragraph.style.display = "none";
+  }
+}
